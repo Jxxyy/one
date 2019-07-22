e21d589c454c0d53bdc76b38a514b5e01aa1db11
Jxxyy <smtliuguiqian@163.com>
2019-07-22 20:29:19 +0800

    sec接口

--- sign/urls.py
+++ sign/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
 	url(r'^get_guest_list/',views_if.get_guest_list,name='get_guest_list'),
 # ex : /api/user_sign/
 	url(r'^user_sign/',views_if.user_sign,name='user_sign')
-]
+	# security interface:
+# ex : /api/sec_get_event_list/
+	url(r'^sec_get_event_list/', views_if_sec.get_event_list,name='get_event_list'),
+	]
